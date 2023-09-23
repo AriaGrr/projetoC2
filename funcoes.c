eb592b66d33016563d0e5313c2a73d07eebfb30f
Roth <rothgorham@gmail.com>
2023-09-23 11:57:49 -0300

    correcao com erros

--- funcoes.c
+++ funcoes.c
@@ -7,28 +7,24 @@
 
 // Função que imprime o menu principal
 void menu(){
-wprintf(L"Menu principal\n");
-wprintf(L"1 - Cadastrar cliente\n");
-wprintf(L"2 - Apagar cliente\n");
-wprintf(L"3 - Listar clientes\n");
-wprintf(L"4 - Débito\n");
-wprintf(L"5 - Depósito\n");
-wprintf(L"6 - Extrato\n");
-wprintf(L"7- Transferência entre contas\n");
-wprintf(L"0 - Sair\n");
-wprintf(L"Digite a opção desejada: ");
+    printf("Menu principal\n");
+    printf("1 - Cadastrar cliente\n");
+    printf("2 - Apagar cliente\n");
+    printf("3 - Listar clientes\n");
+    printf("4 - Débito\n");
+    printf("5 - Depósito\n");
+    printf("6 - Extrato\n");
+    printf("7 - Transferência entre contas\n");
+    printf("0 - Sair\n");
+    printf("Digite a opção desejada: ");
 }
 
 int cadastrarCliente(Clientes *listas, Conta *contas){
     printf("Cadastrar cliente:\n");
 
-    // Limpando o buffer do teclado antes de ler a categoria
-    while (getchar() != '\n');
-
     char nome[100];
     printf("Digite o nome do cliente: ");
     scanf("%s", nome);
-
 
     // Verifica se o nome contém apenas letras
     while (strspn(nome, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") != strlen(nome)) {
@@ -36,7 +32,7 @@ int cadastrarCliente(Clientes *listas, Conta *contas){
         scanf("%s", nome);
     }
 
-    char cpf[11];
+    char cpf[12]; // Incluindo espaço para o caractere nulo '\0'
     printf("Digite o CPF do cliente: ");
     scanf("%s", cpf);
 
@@ -44,154 +40,146 @@ int cadastrarCliente(Clientes *listas, Conta *contas){
     while (strlen(cpf) != 11) {
         printf("CPF inválido. Informe um CPF com 11 dígitos: ");
         scanf("%s", cpf);
-        while (strspn(cpf, "0123456789") != strlen(cpf)) {
-            printf("Cpf invalido. O cpf deve conter apenas números: ");
-            scanf("%s", cpf);
-        }
     }
 
-    char senha[6];
+    char senha[7]; // Incluindo espaço para o caractere nulo '\0'
     printf("Digite a senha do cliente: ");
     scanf("%s", senha);
 
+    // Validar que a senha informada tem 6 dígitos
     while (strlen(senha) != 6) {
-        printf("Senha invalida. digite uma senha com seis digitos númericos: ");
+        printf("Senha inválida. Informe uma senha com seis dígitos numéricos: ");
         scanf("%s", senha);
-            while (strspn(senha, "0123456789") != strlen(senha)) {
-        printf("Senha invalida. A senha deve conter apenas números: ");
-        scanf("%s", senha);
-    }
     }
 
     int tipo;
-    printf("Escolha o tipo de conta: ");
-    printf("1 - Comum\n");
-    printf("2 - Plus\n");
-    scanf("%s", tipo);
+    printf("Escolha o tipo de conta (1 - Comum, 2 - Plus): ");
+    scanf("%d", &tipo);
 
-    while (tipo != '1' && tipo != '2') {
+    while (tipo != 1 && tipo != 2) {
         printf("Tipo de conta inválido. Escolha 1 para conta comum ou 2 para conta plus: ");
-        scanf("%s", tipo);
+        scanf("%d", &tipo);
     }
 
     float saldo;
     printf("Digite o saldo inicial: ");
     scanf("%f", &saldo);
 
-    // Converte o saldo para uma string
-    char saldo_str[100000000];
-    sprintf(saldo_str, "%f", saldo);
     while (saldo < 0) {
         printf("Saldo inválido. Informe um valor maior ou igual a zero: ");
         scanf("%f", &saldo);
-        // Verifica se o saldo contém apenas números
-        for (int i = 0; i < strlen(saldo_str); i++) {
-            if (!isdigit(saldo_str[i])) {
-                printf("Saldo inválido. O saldo deve conter apenas números: ");
-                scanf("%f", &saldo);
-                break;
-            }
-        }
     }
 
-    float saldo = atof(saldo_str);
-
-    // Colocar na lista de contas pelo ponteiro
+    // Adicionar o cliente à lista de contas
 
     return 0;
-    }
+}
 
 int apagarCliente(Clientes *t){
     printf("Apagar cliente:\n");
-    char cpf[11];
+    char cpf[12]; // Incluindo espaço para o caractere nulo '\0'
     printf("Digite o CPF do cliente: ");
     scanf("%s", cpf);
-    printf("CPF: %s\n", cpf);
-    printf("Você tem certeza que deseja apagar o cliente? (s/n): ");
+
     char opcao;
-    scanf("%s", opcao);
-    if (opcao == 's') {
+    printf("Você tem certeza que deseja apagar o cliente? (s/n): ");
+    scanf(" %c", &opcao);
+
+    if (opcao == 's' || opcao == 'S') {
+        //  falta a lógica para apagar o cliente
         printf("Cliente apagado com sucesso!\n");
     } else {
         printf("Operação cancelada.\n");
     }
     return 0;
-    }
+}
 
-// Lista todos os clientes cadastrados dividida por tipo de conta
+// Lista todos os clientes cadastrados divididos por tipo de conta
 int listarClientes(Clientes t){
     printf("Lista de clientes:\n");
-    // for
+    //  falta a lógica para listar os clientes
     return 0;
 }
 
 int debito(Clientes *t){
-    char cpf[11];
-    wprintf(L"Débito:\n");
+    char cpf[12]; // Incluindo espaço para o caractere nulo '\0'
+    printf("Débito:\n");
     printf("Digite o CPF do cliente: ");
     scanf("%s", cpf);
 
-    char senha[6];
+    char senha[7]; // Incluindo espaço para o caractere nulo '\0'
     printf("Digite a senha do cliente: ");
     scanf("%s", senha);
 
+    //  falta a lógica para realizar o débito
+
     return 0;
-    }
+}
 
 int deposito(Clientes *t){
-    wprintf(L"Déposito:\n");
-    char cpf[11];
+    char cpf[12]; // Incluindo espaço para o caractere nulo '\0'
+    printf("Depósito:\n");
     printf("Digite o CPF do cliente: ");
     scanf("%s", cpf);
 
     float valor;
-    printf("Digite o valor do déposito: ");
-    scanf("%f", valor);
+    printf("Digite o valor do depósito: ");
+    scanf("%f", &valor);
+
+    //  falta a para realizar o depósito
 
     return 0;
-    }
+}
 
 // Extrato - gera um arquivo com o histórico de todas as operações realizadas na conta, com datas e valores, incluindo as tarifas.
 int extrato(Clientes t){
+    char cpf[12]; // Incluindo espaço para o caractere nulo '\0'
+    char senha[7]; // Incluindo espaço para o caractere nulo '\0'
+    
     printf("Extrato:\n");
-    char cpf[11];
     printf("Digite o CPF do cliente: ");
     scanf("%s", cpf);
 
-    char senha[6];
     printf("Digite a senha do cliente: ");
     scanf("%s", senha);
+
+    // TODO: Implementar a lógica para gerar o extrato
 
     return 0;
-    }
+}
 
-//  Transferência - realiza a transferência de uma valor determinado de uma conta (Origem) para outra conta (Destino)
+// Transferência - realiza a transferência de um valor determinado de uma conta (Origem) para outra conta (Destino)
 int transferencia(Clientes *t){
-    wprintf(L"Transferência:\n");
-    char cpf[11];
-    printf("Digite o CPF do cliente: ");
-    scanf("%s", cpf);
+    char cpfOrigem[12]; // Incluindo espaço para o caractere nulo '\0'
+    char senha[7]; // Incluindo espaço para o caractere nulo '\0'
+    char cpfDestino[12]; // Incluindo espaço para o caractere nulo '\0'
+    float valor;
 
-    char senha[6];
-    printf("Digite a senha do cliente: ");
+    printf("Transferência:\n");
+    printf("Digite o CPF da conta de origem: ");
+    scanf("%s", cpfOrigem);
+
+    printf("Digite a senha da conta de origem: ");
     scanf("%s", senha);
 
-    printf("Digite o CPF do destinatário: ");
-    scanf("%s", cpf);
+    printf("Digite o CPF da conta de destino: ");
+    scanf("%s", cpfDestino);
 
-    float valor;
     printf("Digite o valor da transferência: ");
-    scanf("%f", valor);
+    scanf("%f", &valor);
 
-    }
+    // falta a lógica para realizar a transferência
+
+    return 0;
+}
 
 int tipoConta(char tipo){
     int taxa, negativo;
-    if (tipo == "1"){
+    if (tipo == 1){
         taxa = 5;
         negativo = 1000;
     }
-    else if (tipo == "2"){
+    else if (tipo == 2){
         taxa = 3;
         negativo = 5000;
     }
@@ -209,9 +197,7 @@ int salvar(Clientes t, char nome[]) {
         return 1;
     }
 
-    // for (int i = 0; i < t.qtd; i++) {
-    //     fwrite(&t.contas[i], sizeof(Conta), 1, arquivo);
-    // }
+    // TODO: Implementar a lógica para salvar as contas no arquivo
 
     fclose(arquivo);
     return 0;
@@ -228,12 +214,37 @@ int carregar(Clientes *t, char nome[]) {
         return 1;
     }
 
-    // t->qtd = 0; // Inicialize o contador de contas carregadas
+  // Carrega os clientes de um arquivo
+// fread
+int carregar(Clientes *t, char nome[]) {
+    FILE *arquivo;
+    arquivo = fopen(nome, "rb");
 
-    // while (fread(&t->contas[t->qtd], sizeof(Conta), 1, arquivo) == 1) {
-    //     t->qtd++;
-    // }
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
 
-    // fclose(arquivo);
-    // return 0;
+    // Lê a quantidade de contas do arquivo
+    fread(&t->qtd, sizeof(size_t), 1, arquivo);
+
+    // Aloca memória para o array de contas
+    t->contas = (Conta *)malloc(t->qtd * sizeof(Conta));
+
+    if (t->contas == NULL) {
+        printf("Erro ao alocar memória.\n");
+        fclose(arquivo);
+        return 1;
+    }
+
+    // Lê as contas do arquivo
+    fread(t->contas, sizeof(Conta), t->qtd, arquivo);
+
+    fclose(arquivo);
+    return 0;
+}
+
+
+    fclose(arquivo);
+    return 0;
 }
--- main.c
+++ main.c
@@ -2,10 +2,17 @@
 #include <stdlib.h>
 #include "funcoes.h"
 
-int main(){
+int main() {
     int cod;
     char arquivo[] = "clientes";
     Clientes t;
+
+    // Criando uma estrutura Conta e um ponteiro para ela
+    Conta conta; // Declaração de uma variável do tipo Conta
+    Conta *ponteiro_para_contas; // Declaração de um ponteiro para Conta
+
+    // Atribuindo o endereço da variável conta ao ponteiro ponteiro_para_contas
+    ponteiro_para_contas = &conta;
 
     cod = carregar(&t, arquivo);
     if (cod == 1) {
@@ -14,58 +21,57 @@ int main(){
 
     int opcao;
 
-    do{    
+    do {    
         menu();
         scanf("%d", &opcao);
         printf("%d\n", opcao);
-    if(opcao == 0){
-        printf("Saindo...\n");
-    }
-    else if (opcao == 1){
-        cadastrarCliente(&listas, &contas);
-        system("clear");
-    }
-    else if (opcao == 2){
-        apagarCliente(&t);
-        system("clear");
-    }
-    else if (opcao == 3){
-        system("clear");
-        listarClientes(t); 
-        printf("Pressione Enter para voltar ao menu...");
-        getchar(); // Captura o Enter
-        getchar(); // Aguarda o próximo Enter
-
-        system("clear");
-    }
-    else if (opcao == 4){
-        debito(&t);
-        system("clear");
-    }
-    else if (opcao == 5){
-        deposito(&t);
-        system("clear");
-    }
-    else if (opcao == 6){
-        system("clear");
-        extrato(t);
-        printf("Pressione Enter para voltar ao menu...");
-        getchar(); // Captura o Enter
-        getchar(); // Aguarda o próximo Enter
-
-        system("clear");
-    }
-    else if (opcao == 7){
-        transferencia(&t);
-        system("clear");
-    }
-     else {
-            wprintf(L"Opção não existe!\n");
+        
+        if (opcao == 0) {
+            printf("Saindo...\n");
+        }
+        else if (opcao == 1) {
+            cadastrarCliente(&t, ponteiro_para_contas);
+            system("clear");
+        }
+        else if (opcao == 2) {
+            apagarCliente(&t);
+            system("clear");
+        }
+        else if (opcao == 3) {
+            system("clear");
+            listarClientes(t); 
+            printf("Pressione Enter para voltar ao menu...");
+            getchar(); // Captura o Enter
+            getchar(); // Aguarda o próximo Enter
+            system("clear");
+        }
+        else if (opcao == 4) {
+            debito(&t);
+            system("clear");
+        }
+        else if (opcao == 5) {
+            deposito(&t);
+            system("clear");
+        }
+        else if (opcao == 6) {
+            system("clear");
+            extrato(t);
+            printf("Pressione Enter para voltar ao menu...");
+            getchar(); // Captura o Enter
+            getchar(); // Aguarda o próximo Enter
+            system("clear");
+        }
+        else if (opcao == 7) {
+            transferencia(&t);
+            system("clear");
+        }
+        else {
+            printf("Opção não existe!\n");
         }
     } while (opcao != 0);
 
     cod = salvar(t, arquivo);
-    if (cod != 0){
+    if (cod != 0) {
         printf("Erro ao salvar!\n");
     }
     return 0;
